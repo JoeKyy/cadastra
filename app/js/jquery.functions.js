659b0c6463ab4ace1c13c9ec846e575feef4adc8
Jhomar Nando <contato@joekyy.com.br>
2019-07-02 14:25:02 -0300

    Correções do cliente

--- app/js/jquery.functions.js
+++ app/js/jquery.functions.js
@@ -4,4 +4,14 @@ $(function () {
         $('.form--content form').show();
         $('.showForm').hide();
     });
+
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if ( scrollTop > 90 ) {
+            $('.form--content > div').addClass("sticky");
+        } else {
+            $('.form--content > div').removeClass("sticky");
+        }
+    });
+
 });
